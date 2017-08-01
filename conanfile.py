0e9e79bd96b524c2d1f6b4d481f80f605a9ce70f
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-01 17:33:25 -0400

    added package info header-only flag

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class BoostWinapiConan(ConanFile):
     version = "1.64.0"
     generators = "txt"
     url = "https://github.com/boostorg/winapi"
-    description = "For a description of this library, please visit http://boost.org/winapi "
+    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "winapi"
     requires =  "Boost.Config/1.64.0@bincrafters/testing", \
@@ -16,5 +16,8 @@ class BoostWinapiConan(ConanFile):
                  .format(self.version, self.url))
 
     def package(self):
-        include_dir = src=os.path.join(os.getcwd(), self.lib_short_name, "include")
+        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
         self.copy(pattern="*", dst="", src=include_dir)
+
+    def package_id(self):
+        self.info.header_only()
