aeacc3ada24b0645f766271ce5ce7b52a0a81e21
Jerry W <jerrywiltse@gmail.com>
2017-08-01 09:50:18 -0400

    updated requires

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,9 @@ class BoostWinapiConan(ConanFile):
     description = "For a description of this library, please visit http://boost.org/winapi "
     license = "www.boost.org/users/license.html"
     lib_short_name = "winapi"
-
+    requires =  "Boost.Config/1.64.0@bincrafters/testing", \
+                      "Boost.Predef/1.64.0@bincrafters/testing"
+    
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
                  .format(self.version, self.url))
