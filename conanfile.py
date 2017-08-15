8d9046e4c670c9a164ac04e607a3da38f27aae41
Jerry W <jerrywiltse@gmail.com>
2017-08-15 16:16:44 -0400

    mass update - change short_name to array - also add user_info for generator

--- conanfile.py
+++ conanfile.py
@@ -7,17 +7,19 @@ class BoostWinapiConan(ConanFile):
     source_url = "https://github.com/boostorg/winapi"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
-    lib_short_name = "winapi"
+    lib_short_names = ["winapi"]
     requires =  "Boost.Config/1.64.0@bincrafters/testing", \
                       "Boost.Predef/1.64.0@bincrafters/testing"
     
     def source(self):
-        self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.source_url))
+        for lib_short_name in self.lib_short_names:
+            self.run("git clone --depth=50 --branch=boost-{0} https://github.com/boostorg/{1}.git"
+                     .format(self.version, lib_short_name)) 
 
     def package(self):
-        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
-        self.copy(pattern="*", dst="include", src=include_dir)
+        for lib_short_name in self.lib_short_names:
+            include_dir = os.path.join(lib_short_name, "include")
+            self.copy(pattern="*", dst="include", src=include_dir)		
 
     def package_id(self):
         self.info.header_only()
