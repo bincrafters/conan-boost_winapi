e06568737a226d71aed11fd75e7a560a1e0870c1
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:35:33 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -2,14 +2,14 @@ from conans import ConanFile, tools, os
 
 class BoostWinapiConan(ConanFile):
     name = "Boost.Winapi"
-    version = "1.64.0"
+    version = "1.65.1"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-winapi"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["winapi"]
-    requires =  "Boost.Config/1.64.0@bincrafters/testing", \
-                      "Boost.Predef/1.64.0@bincrafters/testing"
+    requires =  "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Predef/1.65.1@bincrafters/testing"
     
     def source(self):
         boostorg_github = "https://github.com/boostorg"
