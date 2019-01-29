11ec1401df4dbb8c693667a031d1f644a058d94b
Jerry W <jerrywiltse@gmail.com>
2019-01-28 21:37:57 -0500

    add explicit version numbers, add clang 7, fix travis badges, rename .travis dir to .ci, set python 3.6 on travis and appveyor

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.69.0@bincrafters/testing")
 
 class BoostWinapiConan(base.BoostBaseConan):
     name = "boost_winapi"
+    version = "1.69.0"
     url = "https://github.com/bincrafters/conan-boost_winapi"
     lib_short_names = ["winapi"]
     header_only_libs = ["winapi"]
